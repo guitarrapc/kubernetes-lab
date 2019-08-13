7544c94ae2494afacaf2379f92a603545fa0ed4e
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2019-08-13 18:07:10 +0900

    feat: use k8s on wsl

---
+++ wsl/use_kubectl_on_wsl.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# enable docker and k8s on windows and enable "Expose daemon on tcp://localhost:2375 without TLS"
+# make sure k8s is running on Windows, if not begin reset docker.
+
+# make sure docker can run on wsl
+docker ps 
+
+# install kubeadm
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+sudo apt update
+sudo apt install kubeadm -y
+
+# switch to docker-desktop k8s cluster
+kubectl config use-context docker-desktop
+kubectl cluster-info
+
+# try k8s from wsl
+kubectl run hello-world --image k8s.gcr.io/echoserver:1.10 --port 8080
+kubectl get svc
+kubectl get pods
+kubectl get deployment
+kubectl expose deployment hello-world --type NodePort
+kubectl get svc
+curl http://localhost:30448
+
+# remove pods
+kubectl delete deployment hello-world
+kubectl delete svc hello-world
