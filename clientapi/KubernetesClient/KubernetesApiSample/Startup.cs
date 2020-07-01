9d735031311627530e03ac85640047f32c391476
Ikiru Yoshizaki <3856350+guitarrapc@users.noreply.github.com>
2020-07-01 19:17:21 +0900

    feat: change to multi project watch

--- clientapi/KubernetesClient/KubernetesApiSample/Startup.cs
+++ clientapi/KubernetesClient/KubernetesApiSample/Startup.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-//using KubernetesClient;
+using KubernetesClient;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -28,7 +28,7 @@ namespace KubernetesApiSample
         {
             services.AddControllers();
 
-            //services.AddSingleton<KubernetsApi>();
+            services.AddSingleton<KubernetsApi>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
